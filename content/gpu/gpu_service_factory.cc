3fb14c58593541f0111643ab069daa1399f85002
junweifu <junwei.fu@intel.com>
2019-09-24 09:47:25 +0800

    [Rebase] to Chromium 79.0.3917

--- content/gpu/gpu_service_factory.cc
+++ content/gpu/gpu_service_factory.cc
@@ -91,8 +91,8 @@ void GpuServiceFactory::RunService(
 
   if (service_name == ml::mojom::kServiceName) {
     scoped_refptr<base::SingleThreadTaskRunner> task_runner =
-        base::CreateSingleThreadTaskRunnerWithTraits(
-            {base::TaskPriority::USER_BLOCKING});
+        base::CreateSingleThreadTaskRunner(
+            {base::ThreadPool(), base::TaskPriority::USER_BLOCKING});
     task_runner->PostTask(
         FROM_HERE,
         base::BindOnce(
