799d138de170113c9116001d5ea6f4cd3bcb9bc0
Shiyi Zou <shiyi.zou@intel.com>
2024-12-20 11:03:42 +0800

    Replace protobuf with OrtGraph API

--- gpu/config/gpu_switches.cc
+++ gpu/config/gpu_switches.cc
@@ -175,4 +175,6 @@ const char kEnableGpuMainTimeKeeperMetrics[] =
 // log expectation comparisons.
 const char kSuppressPerformanceLogs[] = "suppress-performance-logs";
 
+const char kUseRedistributableONNXRuntime[] = "use-redist-ort";
+
 }  // namespace switches
