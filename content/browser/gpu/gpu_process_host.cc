15a38b0b874001251e2f187cf93d2bfab4cc04cd
mingmingtasd <mingming1.xu@intel.com>
2025-02-25 15:40:20 +0800

    introduce "--webnn-ort-use-ov-gpu-fp32" (#160)
    This switch will force OV EP to use FP32 inference precision for GPU to
    get better accuracy, but it may result in decreased performance.

--- content/browser/gpu/gpu_process_host.cc
+++ content/browser/gpu/gpu_process_host.cc
@@ -334,6 +334,7 @@ static const char* const kSwitchNames[] = {
     switches::kWebNNOrtDumpModel,
     switches::kWebNNOrtUseOpenvino,
     switches::kWebNNOrtDisableCpuFallback,
+    switches::kWebNNOrtUseOVGpuFP32,
 #endif
 };
 
